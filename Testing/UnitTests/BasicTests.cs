feebc69a2da8956434afc9fd91d54bc29fbcdb69
andykras <root@andykras.org>
2016-01-05 05:58:26 +0300

    Added UnitTests

---
+++ Testing/UnitTests/BasicTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using TetrisModel;
+
+namespace CompositeUnitTests
+{
+  [TestFixture()]
+  public class BasicTests
+  {
+    [Test()]
+    public void ColorsTest()
+    {
+      Assert.AreEqual((int) ConsoleColor.Black, (int) Color.Black);
+    }
+
+    [Test()]
+    public void HierarchyTest()
+    {
+      var root = new CompositeUnit();
+      var unit = new CompositeUnit();
+      var leaf1 = new Sprite();
+      unit.AddUnit(leaf1);
+      root.AddUnit(unit);
+      ;
+      var leaf2 = new Sprite();
+      root.AddUnit(leaf2);
+      Assert.IsTrue(root.Enable);
+    }
+  }
+}
+
